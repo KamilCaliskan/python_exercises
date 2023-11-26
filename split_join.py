ea9e3743ae208f95fe9e0f624a5605d024f44d2c
kamil Çalışkan <37404388+KamilCaliskan@users.noreply.github.com>
2023-11-26 15:42:06 +0300

    split_join.py

---
+++ split_join.py
@@ -0,0 +1,19 @@
+
+
+
+
+def split_and_join(line):
+    # Split the input string on a space delimiter
+    words = line.split(" ")
+
+    # Join the words using a hyphen
+    result = "-".join(words)
+
+    return result
+
+# Sample Input
+input_string = "this is a string"
+
+# Sample Output
+output_string = split_and_join(input_string)
+print(output_string)
