febb0e78bf9799c7ba02f6645ce5b764662b4f53
kamil CALISKAN <37404388+KamilCaliskan@users.noreply.github.com>
2024-03-20 06:05:33 +0100

    Create divmod.py

---
+++ Math/divmod.py
@@ -0,0 +1,16 @@
+'''
+One of the built-in functions of Python is divmod, 
+which takes two arguments A and B and returns a tuple containing the quotient of 
+A/B first and then the remainder A
+'''
+
+
+a = int(input())
+b = int(input())
+
+print(a // b)
+
+
+print(a % b)
+
+print(divmod(a, b))
